2b7c84a5dff2dbf9ddae076f3890dfbe609a92e0
Gonzalo Delgado <gonzalodel@gmail.com>
2012-09-29 14:38:35 -0300

    Primer commit

---
+++ batallatv3/local_settings.py
@@ -0,0 +1,17 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'batallatv3',                      # Or path to database file if using sqlite3.
+    }
+}
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
